aaf3e1b53057d452a685488b412a5d73bbcce4f1
austinbaggetta <austin.baggetta@icahn.mssm.edu>
2025-05-05 14:28:29 -0400

    Detect cells using local maximums

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,9 @@
 import os
 import cv2
 import math
+import pickle
 import numpy as np
+import pandas as pd
 import dask.array as da
 import xarray as xr
 from os.path import join as pjoin
@@ -17,9 +19,13 @@ def choose_file(image_path, pattern):
         else:
             pass
 
-def load_image(dpath, pattern, dtype=np.uint8, varr_name=None):
+
+def load_image(dpath, pattern, varr_name=None):
     image_path = choose_file(dpath, pattern)
-    im = cv2.imread(image_path, cv2.IMREAD_ANYDEPTH) ## may need to do cv2.IMREAD_GRAYSCALE
+    im = cv2.imread(image_path, cv2.IMREAD_ANYDEPTH)
+    ## openCV requires images be 8-bit
+    if im.dtype == np.uint16:
+        im = (im/256).astype(np.uint8)
     darr = da.from_array(im, chunks=-1)
     varr = xr.DataArray(darr,
                         dims=['height', 'width'],
@@ -27,8 +33,6 @@ def load_image(dpath, pattern, dtype=np.uint8, varr_name=None):
                             height=np.arange(0, darr.shape[0]),
                             width=np.arange(0, darr.shape[1])
                         ))
-    if dtype:
-        varr = varr.astype(dtype)
     if varr_name is not None:
         varr = varr.rename(varr_name)
     else:
@@ -62,16 +66,19 @@ def remove_background(image: xr.DataArray, method: str, wnd: int) -> xr.DataArra
             'tophat': This operation returns the bright spots of the image that are smaller
                       than the structuring element.
     """
+    kernel = disk(wnd)
     if method == 'tophat':
-        kernel = disk(wnd)
         res = cv2.morphologyEx(image.values, cv2.MORPH_TOPHAT, kernel)
-        res = xr.DataArray(res,
-                           dims=['height', 'width'],
-                           coords=dict(
-                                height=np.arange(0, res.shape[0]),
-                                width=np.arange(0, res.shape[1])
-                            )).chunk(chunks=-1)
-        return res.rename('background_subtracted')
+    elif method == 'opening':
+        res = cv2.morphologyEx(image.values, cv2.MORPH_OPEN, kernel)
+
+    res = xr.DataArray(res,
+                        dims=['height', 'width'],
+                        coords=dict(
+                            height=np.arange(0, res.shape[0]),
+                            width=np.arange(0, res.shape[1])
+                        )).chunk(chunks=-1)
+    return res.rename('background_subtracted')
 
 
 def detect_cells(image, contours, contour_color=100, minimum_area=600, maximum_area=2500, average_cell_area=1000, connected_cell_area=600):
@@ -122,5 +129,77 @@ def calculate_threshold(im, thresh_type='otsu'):
         func = threshold_otsu
     elif thresh_type == 'mean':
         func = threshold_mean
-    
     return func(values)
+
+
+def local_max(im, wnd_dist, diff):
+    k = disk(wnd_dist)
+    im_max = cv2.dilate(im, k)
+    im_min = cv2.erode(im, k)
+    im_diff = ((im_max - im_min) > diff).astype(np.uint8)
+    im_ext = (im == im_max).astype(np.uint8)
+    return cv2.bitwise_and(im_ext, im_diff).astype(np.uint8)
+
+
+def single_seeds(lmax):
+    """ 
+    Creates a single seed at the median location for all the local maximum that are connected.
+    """
+    nlab, max_lab = cv2.connectedComponents(lmax)
+    max_res = np.zeros_like(lmax)
+    for lb in range(1, nlab):
+        area = max_lab == lb
+        if np.sum(area) > 1:
+            crds = tuple(int(np.median(c)) for c in np.where(area))
+            max_res[crds] = 1
+        else:
+            max_res[np.where(area)] = 1
+    return max_res.astype(float)
+
+
+def combine_seeds(max_res, dist_thresh=4):
+    """ 
+    Combines seeds that are within a certain pixel distance from each other.
+    """
+    seeds = np.where(max_res == 1)
+    seed_df = pd.DataFrame({'seed': np.arange(0, seeds[0].shape[0]), 'x': seeds[0], 'y': seeds[1]})
+    seeds_final = pd.DataFrame(columns=['seed', 'x', 'y'])
+    duplicate_seeds = []
+    for seed in np.arange(0, seeds[0].shape[0]):
+        x_dif = seed_df['x'] - seed_df.loc[seed, 'x']
+        y_dif = seed_df['y'] - seed_df.loc[seed, 'y']
+        dist = np.hypot(x_dif, y_dif)
+        if any((dist <= dist_thresh) & (dist > 0)):
+            neighbor = np.where((dist <= dist_thresh) & (dist > 0))[0][0]
+            same_seed = seed_df.loc[[seed, neighbor]]
+            new_x, new_y = int(np.mean(same_seed['x'])), int(np.mean(same_seed['y']))
+        else:
+            new_x, new_y = seed_df.loc[seed, 'x'], seed_df.loc[seed, 'y']
+            neighbor = np.nan
+        
+        if seed in duplicate_seeds:
+            pass
+        elif neighbor in duplicate_seeds:
+            pass
+        else:
+            seeds_final.loc[seed, ['seed', 'x', 'y']] = [seed, new_x, new_y]
+        
+        if ~np.isnan(neighbor):
+            duplicate_seeds.append(neighbor)
+        
+    cell_loc = np.zeros_like(max_res)
+    for seed in seeds_final['seed']:
+        cell_loc[seeds_final['x'][seeds_final['seed'] == seed].values[0], seeds_final['y'][seeds_final['seed'] == seed].values[0]] = 1
+    return cell_loc, seeds_final
+
+
+def save_params(median_params, bg_sub_params, local_max_params, combine_seeds_params, spath):
+    all_params = {
+        'median_params': median_params,
+        'bg_sub_params': bg_sub_params,
+        'local_max_params': local_max_params,
+        'combine_seeds_params': combine_seeds_params
+    }
+
+    with open(pjoin(spath), 'wb') as file:
+        pickle.dump(all_params, file, protocol=pickle.HIGHEST_PROTOCOL)
